7bb1839d6b5b846370f5af622ad8e7d980a85697
andii04 <36232071+andii04@users.noreply.github.com>
2018-02-07 16:20:40 +0100

    Update main.c
    meine ersten versuche mal hochgeladen, vielleicht ist was wertvolles dabei :D

--- main.c
+++ main.c
@@ -1,9 +1,103 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
+//Globale Variablen
+int anzSpieler;
+int aktSpieler=1;
+int aktWuerfel[];
+int saveWuerfel[];
 int main()
 {
-    printf("Hello world!\n");
-    printf("sfa saf");
+
+    init();
+    wuerfeln();
+    AnzeigeSpieler();
     return 0;
 }
+
+
+    void init() {
+
+    printf("Geben Sie die Anzahl der Spieler ein: ");
+
+    scanf("%i", &anzSpieler);
+
+    printf("Anzahl der Spieler: %i\n", anzSpieler);
+
+    char spielername [anzSpieler][30];
+
+    printf("Bitte geben Sie %i Namen ein:\n", anzSpieler);
+
+    for (int i = 0; i < anzSpieler; i++) {
+        printf("spielername[%i] = ",i);
+        scanf("%s", &spielername[i]);
+    }
+
+    printf("\nTesten\n\n");
+
+    for(int i = 0; i < anzSpieler; i++) {
+        printf("%s\n", spielername[i]);
+    }
+}
+
+// the random function
+void RandomNumberGenerator(const int nMin,const int nMax,const int  nNumOfNumsToGenerate)
+{
+int nRandomNumber = 0;
+for (int i = 0; i < nNumOfNumsToGenerate; i++)
+{
+nRandomNumber = rand()%(nMax-nMin) + nMin;
+aktWuerfel[i]= nRandomNumber;
+printf("%d ", aktWuerfel[i]);
+}
+printf("\n");
+printf("%d\n",aktWuerfel[0]);
+}
+
+void generate()
+{
+srand(time(NULL));
+RandomNumberGenerator(1,7,5);
+}
+
+void wuerfeln()
+{
+    generate();
+
+}
+
+void AnzeigeSpieler()
+{
+
+    int antwort;
+    printf("Spielername[i]");
+    printf("Nochmal neu wuerfeln ? 0 = no/1 = yes?");
+    scanf("%i", &antwort);
+    if(antwort == 0)
+    {
+        //wuerfeln();
+        printf("Wie viel Wuerfel möchtest du speichern? zwischen 1-4");
+        scanf("%i",&antwort);
+        if(antwort <=0 || antwort >4)
+        {
+            printf("Wie viel Wuerfel möchtest du speichern? zwischen 1-4");
+        }
+        else if(antwort>0&& antwort <5)
+        {
+            for(int i = 0;i<antwort;i++)
+            printf("Welche Wuerfelnummer willst du speichern ? 0-4");
+           // scanf("%d",&saveWuerfel[i]);
+        }
+
+    }
+    else if(antwort == 1)
+    {
+        wuerfeln();
+
+
+    }
+
+}
+
